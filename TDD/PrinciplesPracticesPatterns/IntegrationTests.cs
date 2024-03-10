54b749530d4781f7124b55405168ad20e860dc4a
Reza <mrdev1020@gmail.com>
2024-03-10 11:50:21 +0330

    Setup integration tests

---
+++ TDD/PrinciplesPracticesPatterns/IntegrationTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+
+namespace PrinciplesPracticesPatterns;
+
+public class IntegrationTests : IClassFixture<WebApplicationFactory<Program>>
+{
+    private readonly WebApplicationFactory<Program> factory;
+
+    public IntegrationTests(WebApplicationFactory<Program> factory)
+    {
+        this.factory = factory;
+    }
+
+    [Theory]
+    [InlineData("/")]
+    [InlineData("/Contact")]
+    public async Task Get_EndpointsReturnSuccessAndCorrectContentType(string url)
+    {
+        // Arrange
+
+        var client = factory.CreateClient();
+
+        // Act
+
+        var response = await client.GetAsync(url);
+
+        // Assert
+
+        Assert.Equal("text/html; charset=utf-8", response.Content.Headers.ContentType.ToString());
+    }
+}
