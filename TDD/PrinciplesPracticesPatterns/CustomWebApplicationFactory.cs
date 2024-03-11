65171c0ca9c8c3fd7b58f9ba7d601f4d22764250
Reza <mrdev1020@gmail.com>
2024-03-11 10:35:17 +0330

    Mock

--- TDD/PrinciplesPracticesPatterns/CustomWebApplicationFactory.cs
+++ TDD/PrinciplesPracticesPatterns/CustomWebApplicationFactory.cs
@@ -8,37 +8,37 @@ public class CustomWebApplicationFactory<TProgram> : WebApplicationFactory<TProg
 {
     protected override void ConfigureWebHost(IWebHostBuilder builder)
     {
-        builder.ConfigureServices(services =>
-        {
-            var dbContextDescriptor = services.SingleOrDefault(
-                d => d.ServiceType ==
-                    typeof(DbContextOptions<ApplicationDbContext>));
+        //builder.ConfigureServices(services =>
+        //{
+        //    var dbContextDescriptor = services.SingleOrDefault(
+        //        d => d.ServiceType ==
+        //            typeof(DbContextOptions<ApplicationDbContext>));
 
-            services.Remove(dbContextDescriptor);
+        //    services.Remove(dbContextDescriptor);
 
-            var dbConnectionDescriptor = services.SingleOrDefault(
-                d => d.ServiceType ==
-                    typeof(DbConnection));
+        //    var dbConnectionDescriptor = services.SingleOrDefault(
+        //        d => d.ServiceType ==
+        //            typeof(DbConnection));
 
-            services.Remove(dbConnectionDescriptor);
+        //    services.Remove(dbConnectionDescriptor);
 
-            // Create open SqliteConnection so EF won't automatically close it.
-            services.AddSingleton<DbConnection>(container =>
-            {
-                var connection = new SqliteConnection("DataSource=:memory:");
-                connection.Open();
+        //    // Create open SqliteConnection so EF won't automatically close it.
+        //    services.AddSingleton<DbConnection>(container =>
+        //    {
+        //        var connection = new SqliteConnection("DataSource=:memory:");
+        //        connection.Open();
 
-                return connection;
-            });
+        //        return connection;
+        //    });
 
-            services.AddDbContext<ApplicationDbContext>((container, options) =>
-            {
-                var connection = container.GetRequiredService<DbConnection>();
-                options.UseSqlite(connection);
-            });
-        });
+        //    services.AddDbContext<ApplicationDbContext>((container, options) =>
+        //    {
+        //        var connection = container.GetRequiredService<DbConnection>();
+        //        options.UseSqlite(connection);
+        //    });
+        //});
 
         builder.UseEnvironment("Development");
     }
 }
-}
+
---
+++ TDD/PrinciplesPracticesPatterns/Mock.cs
@@ -0,0 +1,48 @@
+﻿using Moq;
+
+namespace PrinciplesPracticesPatterns;
+
+public class Mock
+{
+    [Fact]
+    public void Ping_Invokes_DoSomething()
+    {
+        // Arrange
+
+        var mockIFoo = new Mock<IFoo>();
+        mockIFoo.Setup(i => i.DoSomething(It.IsAny<string>())).Returns(true);
+        var service = new Service(mockIFoo.Object);
+
+        // Act
+
+        service.Ping();
+
+
+        // Assert
+
+        mockIFoo.Verify(i => i.DoSomething("PING"), Times.Once());
+    }
+}
+
+
+
+
+public class Service
+{
+    private readonly IFoo _foo;
+
+    public Service(IFoo foo)
+    {
+        _foo = foo ?? throw new ArgumentNullException(nameof(foo));
+    }
+
+    public void Ping()
+    {
+        _foo.DoSomething("PING");
+    }
+}
+
+public interface IFoo
+{
+    bool DoSomething(string command);
+}
